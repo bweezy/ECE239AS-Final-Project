bbb8723d1884b849723fbbac0488dc48efc84aa9
Arjun Lakshmipathy <lakshmipathyarjun6@gmail.com>
2018-03-04 21:24:33 -0800

    starting with lstm

--- GAN.py
+++ GAN.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential, Model
 from keras.optimizers import Adam
+from keras.layers import Input, LSTM, Dense, Flatten
 
 
 
@@ -11,15 +12,17 @@ class GAN():
 
 	def __init__(self, use_reccurent_gen=False,
 								use_recurrent_disc=False):
-		self.input_size = None
-		self.channels = None
+		self.input_size = 1000
+		self.channels = 22
 
 		self.optimizer = Adam(lr=1e-3, beta_1=.9, beta_2=.99,
 													decay=.99)
 
-
 		self.generator = self.Generator(recurrent=use_reccurent_gen)
+
+
 		self.discriminator = self.Discriminator(recurrent=use_recurrent_disc)
+		self.discriminator.compile(loss='binary_crossentropy', optimizer=self.optimizer, metrics=['accuracy'])
 		
 
 
@@ -35,7 +38,33 @@ class GAN():
 
 	def Discriminator(self, recurrent):
 
+		data_shape = (self.channels, self.input_size)
 
-		# Assuming model is not recurrent
 		model = Sequential()
+		model.add(LSTM(100,input_shape=data_shape))
+		model.add(Dense(1, activation='sigmoid'))
+		print(model.summary())
 
+		inp = Input(shape=data_shape)
+		validity = model(inp)
+
+		return Model(inp, validity)
+
+	def train(self, X, y, epochs, batch_size=128, save_interval=50):
+
+		X_train = X
+		y_train = y
+
+		half_batch = int(batch_size / 2)
+
+		print self.discriminator.train_on_batch(X_train, np.ones(X.shape[0]))
+
+		# for epoch in range(epochs):
+		# 	idx = np.random.randint(0, X_train.shape[0], half_batch)
+		# 	readings = X_train[idx]
+        #
+		# 	noise = np.random.normal(0, 1, (half_batch, 100)) # this is not correct -- needs to be similar to eeg values
+        #
+		# 	gen_readings = noise # this is not correct -- generator should make a prediction
+        #
+		# 	d_loss_real = self.discriminator.fit(readings, np.ones((half_batch, 288)))
